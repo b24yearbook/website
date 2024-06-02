1d71b0aa0500d828e018245d83f2b27986419cb0
Rojiver Pamerol <rojiver232@gmail.com>
2024-06-03 01:29:03 +0800

    started batch gallery pic page

--- pages/gallery/batch_gallery.js
+++ pages/gallery/batch_gallery.js
@@ -1,2 +1,26 @@
-let gallery = document.getElementById("gallery");
+var pageNo = 0;
 
+function setup() {
+    // limit view to 12 at a time
+
+    for (let i = 0; i < 12; i++) {
+        let gallery = document.getElementById("gallery");
+
+        let item = document.createElement("div");
+        item.className = "item";
+
+        let link = document.createElement("a");
+        link.href = "batchg_pic.html";
+
+        let picture = document.createElement("img");
+        picture.src = "../../resources/img/PLACEHOLDER.png";
+        picture.alt = "Batch Picture";
+        //picture.className = "picture";
+
+        let pictureName = document.createElement("p");
+        pictureName.className = "pictureName";
+        pictureName.innerText = "name";
+
+        item.appendChild(link); link.appendChild(picture); item.appendChild(pictureName); gallery.appendChild(item);
+    }
+}
